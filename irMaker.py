cda3b891869dd334792743ed039b95318011cbac
salsdsp <charrington@salesianum.org>
2026-02-05 16:37:49 -0500

    GenIR era
    Added IR maker and a test IR

--- irMaker.py
+++ irMaker.py
@@ -7,31 +7,35 @@ import random as rand
 
 audio = []
 sampleRate = 44100.0
-saveFile = "Waveforms/combiwave_9.wav"
+irLength = 1
+delay = 0.05
+delay = int(sampleRate*delay)
+irLength *= int(sampleRate)
+lfoFreq = 440
+saveFile = "GenIRs/test1.wav"
 msam = 0.0
 
+"""
 def myWave(sample):
     n = 0
     for harm in range(1,2**12):
         n += math.cos(harm*sample)**(11-int(11 * math.cos(harm*sample)/harm**2))/harm**1.5
     return n
+"""
 
 def generate_wave(volume=0.5):
 
     global audio, msam
 
-    for x in range(0,2696):
-        sample = 2 * math.pi * 16.3516 * ( x / sampleRate )
-        temp = volume * myWave(sample)
+    for x in range(0, delay):
+        audio.append(0)
+    for x in range(0,irLength):
+        lfo = 2 * math.pi * math.sin(x*lfoFreq) * ( x / sampleRate )
+        #temp = volume * myWave(sample)
+        temp = rand.random()*((irLength-x)/irLength)**3 * math.sin(lfo)
         if abs(temp) > abs(msam):
             msam = temp
         audio.append(temp)
-        if x == 674:
-            print("25% Complete")
-        elif x == 1348:
-            print("50% Complete")
-        elif x == 2022:
-            print("75% Complete")
     return
 
 
@@ -67,7 +71,7 @@ generate_wave()
 save_wav(saveFile)
 print("Completed without error")
 print("Saved to",saveFile)
-times = np.linspace(0, 2696/sampleRate, 2696)
+times = np.linspace(0, (delay+irLength)/sampleRate, (delay+irLength))
 plt.figure(figsize=(15,6))
 plt.plot(times, audio)
 plt.title('Waveform')
